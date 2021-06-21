75efc3edd698673e6f9e097b5db2d79ffe78a793
Welbert Castro <himewel@users.noreply.github.com>
2021-06-20 21:57:15 -0300

    Remove env file

--- hadoop/start.sh
+++ hadoop/start.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-if [ -z "$CLUSTER_NAME" ]; then
-    echo "Starting datanode"
-    hdfs --config $HADOOP_CONF_DIR datanode
-else
-    echo "Starting namenode"
-    echo "Y" | hdfs --config $HADOOP_CONF_DIR namenode -format -clusterID CID-YYY
-    hdfs --config $HADOOP_CONF_DIR namenode
-fi
+echo "Y" | hdfs namenode -format -clusterID CID-YYY
+hdfs namenode
