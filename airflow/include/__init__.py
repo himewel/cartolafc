efd3bdfd5df5a353139ca2a1aa6f0d3ae3faaaee
Welbert Castro <himewel@users.noreply.github.com>
2021-06-28 21:34:06 -0300

    Replace python call for package imports

--- airflow/include/__init__.py
+++ airflow/include/__init__.py
@@ -1,3 +1,3 @@
-from raw_extractor import RawExtractor
-from scouts_transform import ScoutsTransform
-from rounds_transform import RoundsTransform
+from .raw_extractor import RawExtractor
+from .scouts_transform import ScoutsTransform
+from .rounds_transform import RoundsTransform
