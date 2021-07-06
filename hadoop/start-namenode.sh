f78f17302b1fcb40f1c80bd2a24748a1ee6744c9
Welbert Castro <himewel@users.noreply.github.com>
2021-07-06 20:23:11 -0300

    Persist data on hadoop

--- hadoop/start-namenode.sh
+++ hadoop/start-namenode.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Y" | hdfs namenode -format -clusterID CID-YYY
+namenode_dir=$(ls -A /hadoop/dfs/name)
+if [[ ! -z "$namenode_dir" ]]; then
+    echo "Directory is NOT empty!"
+else
+    echo "Directory is empty!"
+    echo "Y" | hdfs namenode -format -clusterID CID-YYY
+fi
+
 hdfs namenode
