c1d429cd9c2035231f82c023ab4551a1716fb810
Welbert Castro <himewel@users.noreply.github.com>
2021-08-03 10:58:52 -0300

    Add stop target

--- quickstart.sh
+++ quickstart.sh
@@ -1,29 +1,41 @@
 #!/usr/bin/env bash
 
 mode=$1
+
 case "$mode" in
     airflow)
         echo "Starting Apache Airflow services..."
         docker-compose \
             --file docker-compose.airflow.yaml \
-            --file docker-compose.hive.yaml \
             --file docker-compose.datahub.yaml \
+            --file docker-compose.hive.yaml \
             up --detach
         ;;
 
     superset)
         echo "Starting Apache Superset services..."
         docker-compose \
-            --file docker-compose.superset.yaml \
-            --file docker-compose.hive.yaml \
             --file docker-compose.datahub.yaml \
+            --file docker-compose.hive.yaml \
+            --file docker-compose.superset.yaml \
             up --detach
         ;;
 
+    stop)
+        echo "Stopping services..."
+        docker-compose \
+            --file docker-compose.airflow.yaml \
+            --file docker-compose.datahub.yaml \
+            --file docker-compose.hive.yaml \
+            --file docker-compose.superset.yaml \
+            stop
+        ;;
+
     help)
-        echo "Choose a group of services to start [airflow|superset]..."
+        echo "Choose a group of services to start [airflow|superset|stop]..."
         echo "An empty param will start Airflow, Superset, Datahub, Hive and Hadoop together"
         ;;
+
     *)
         echo "Starting all services..."
         docker-compose \
